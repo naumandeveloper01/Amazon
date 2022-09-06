1ad2700f3e7c326a0bdedc69d7a2649f28f39732
naumandeveloper01 <112519771+naumandeveloper01@users.noreply.github.com>
2022-09-06 13:17:23 +0500

    Affiliate products of the amazon

---
+++ app.js
@@ -0,0 +1,7 @@
+// let headtxt = document.querySelector(".headtxt")
+let headcontent = new Typed(".headtxt", {
+    strings: ["Getting The Best Products Of The Amazon Are Available", "Discount Is Available On This Product For A Limited Period"],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true,
+});
